0b6543a756f85847dc2df31d72604c2c5c8f3601
AngeloC89 <Angelociulla89@gmail.com>
2024-05-08 16:11:13 +0200

    Created scaffolding.

---
+++ Views/footer.php
@@ -0,0 +1,7 @@
+<footer class="main">
+    <h2 class="my-5">Footer</h2>
+  
+</footer>
+
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,15 @@
+<?php
+
+include __DIR__ . "/Views/header.php";
+include __DIR__ ."/Models/Movie.php";
+?>
+
+<main>
+
+<h2>main</h2>
+</main>
+
+<?php
+include __DIR__ . "/Views/footer.php";
+?> 
+
