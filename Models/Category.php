5f983edc778e3fcc1908315cdad93be7d6bc67d7
wojciechpotoniec <wojtekpotoniec@hotmail.it>
2024-05-09 12:38:21 +0200

    created function fetchCategories

--- Models/Category.php
+++ Models/Category.php
@@ -1,11 +1,24 @@
 <?php
 
-class Category{
+class Category
+{
     public $name;
     public $logo;
 
-    function __construct($name, $logo){
+    function __construct($name, $logo)
+    {
         $this->name = $name;
         $this->logo = $logo;
+    }
+    public static function fetchCategories()
+    {
+        $data = file_get_contents(__DIR__ . "/categories_db.json");
+        $dataphp = json_decode($data, true);
+        // var_dump($dataphp);
+        $categories = [];
+        foreach ($dataphp as $key => $value) {
+            $categories[] = new Category($value['name'], $value['logo']);
+        }
+        return $categories;
     }
 }
