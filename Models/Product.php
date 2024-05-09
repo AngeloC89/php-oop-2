df816d66f14ae7a54ba0e7c7a5d351a29fa13be7
Nik0-9 <filannino.nicola9@gmail.com>
2024-05-09 19:08:34 +0200

    ADD type of properties of class

--- Models/Product.php
+++ Models/Product.php
@@ -2,10 +2,10 @@
 include_once __DIR__ ."/Category.php";
 
 class Product{
-    public $title;
-    public $price;
-    public $description;
-    public $img;
+    public string $title;
+    public float $price;
+    public string $description;
+    public string $img;
     public Category $category;
 
     public function __construct($category, $title, $price, $description, $img){
